17b3257d9fc711a90b2535969fbfd5974a0caee4
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-04-15 17:14:21 +0900

    postモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_032833) do
+ActiveRecord::Schema.define(version: 2020_04_15_080108) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.string "image", null: false
+    t.string "money"
+    t.string "adress"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
