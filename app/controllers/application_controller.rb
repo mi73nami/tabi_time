dd6ecd19eeee5a95429e65cfd859330f16f69e44
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-04-17 11:00:10 +0900

    カテゴリをテーブルから取得

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def set_category
+    @category = Category.all
+  end
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :userimage, :biography])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_category
   def index
   end
 
