fd49306002d0434aa87a754bff4b1637b1c00b5f
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-07 23:53:45 +0900

    update createアクションエラーハンドリング
    createアクションで投稿ができなかった場合の処理を追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,8 +18,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to root_path 
+    post = Post.new(post_params)
+    if post.save
+      redirect_to root_path 
+    else
+      redirect_to action:"new"
+    end
   end
 
   def show
