2db002b911cd1f64a887ea64e238be386b4b09d8
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-11 21:53:13 +0900

    add アソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ApplicationRecord
   validates :title, :text, :image, presence: true
   
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :category
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
 
   mount_uploader :image, ImageUploader
 end
