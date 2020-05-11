c90ef4b274175e9335fdf06bd7be35a9d69da88e
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-11 23:21:57 +0900

    add post/showにお気に入りボタンを設置
    ★アイコンを押すとお気に入りに登録され、もう一度押すとお気に入りが解除される

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_back(fallback_location: post_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -10,5 +10,5 @@ class LikesController < ApplicationController
     @like.destroy
     redirect_back(fallback_location: root_path)
   end
-  
+
 end
