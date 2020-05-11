90a5e039f00685b06473f5aa38afd68a64959b96
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-11 22:18:02 +0900

    add createアクションを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,6 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.create(post_id: params[:post_id])
+    redirect_back(fallback_location: post_path)
+  end
 end
