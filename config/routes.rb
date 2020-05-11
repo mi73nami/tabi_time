dc39e173fc86c3b76792298b162b9bd4464fd982
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-11 22:07:30 +0900

    add ルーティングの設定
    likeはpostに依存しているため、ネストにする

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   
   resources :posts do
     resources :comments, only: :create
+    resources :likes, only: [:create, :destroy]
   end
 
   resources :users, only: [:show, :edit, :update]
