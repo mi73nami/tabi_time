967fcf84a9defa9fe4288339252729c8f5db093d
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-11 14:34:17 +0900

    update postサムネイル、カテゴリボックスを部分テンプレート化

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
   def show
     @posts = Post.joins(:category).where(category_id: params[:id])
     @category = Category.find(params[:id])
