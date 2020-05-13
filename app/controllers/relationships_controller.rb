371a5f1d724637ff74b08daf0e0411d4be040633
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-13 14:51:43 +0900

    add createアクション
    フォローするときのアクション

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,14 @@
 class RelationshipsController < ApplicationController
   before_action :set_user
+
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
   
   private
   def
