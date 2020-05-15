3a638f7235e7c956841978e679c29134d91db5ff
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-15 15:10:40 +0900

    update place,category表示順
    place,category投稿一覧にて表示順を新着順に設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :set_category, :set_place
   def show
-    @posts = Post.joins(:category).where(category_id: params[:id])
+    @posts = Post.joins(:category).where(category_id: params[:id]).order("created_at DESC")
     @category = Category.find(params[:id])
   end
 end
