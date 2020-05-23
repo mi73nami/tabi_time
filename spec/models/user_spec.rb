e8cd491aa86b9bcf519e58549348fa731192a448
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-23 23:25:08 +0900

    user.rb test nicknameがない場合は登録できないこと
    pass済

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,10 @@ describe User do
   describe '#create' do
 
     it "nicknameがない場合は登録できないこと" do
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("が入力されていません。")
     end
-    
+
   end
 end
