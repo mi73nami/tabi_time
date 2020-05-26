c5bd679a62e33658dfe40050fe7d5866269b809e
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-26 18:13:59 +0900

    fix testエラーメッセージ部修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,19 +5,19 @@ describe User do
     it "nicknameがない場合は登録できないこと" do
       user = build(:user, nickname: "")
       user.valid?
-      expect(user.errors[:nickname]).to include("が入力されていません。")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     it "emailがない場合は登録できないこと" do
       user = build(:user, email: "")
       user.valid?
-      expect(user.errors[:email]).to include("が入力されていません。")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     it "passwordがない場合は登録できないこと" do
       user = build(:user, password: "")
       user.valid?
-      expect(user.errors[:password]).to include("が入力されていません。")
+      expect(user.errors[:password]).to include("を入力してください")
     end
 
     it "nicknameが6文字以下では登録できること " do
