46fbb589e21b20e237c3fb1d4bfc03bdbf6d6fdf
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-29 17:09:44 +0900

    post.rb test titleが無い場合は登録できないこと
    pass済

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+describe Post do
+  describe '#create' do
+
+    it "titleが無い場合は登録できないこと" do
+      post = build(:post, title: "")
+      post.valid?
+      expect(post.errors[:title]).to include("を入力してください")
+    end
+  
+  end
+end
