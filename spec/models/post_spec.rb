acf62866256ac0467f13bc4e082324cc31560a8c
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-29 17:32:28 +0900

    post.rb test 入力欄が全て入力されていた場合は登録できること
    pass済

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,40 +2,56 @@ require 'rails_helper'
 describe Post do
   describe '#create' do
 
-    it "titleが無い場合は登録できないこと" do
-      post = build(:post, title: "")
-      post.valid?
-      expect(post.errors[:title]).to include("を入力してください")
-    end
+    context "postを保存できる場合"do
 
-    it "imageが無い場合は登録できないこと" do
-      post = build(:post, image: "")
-      post.valid?
-      expect(post.errors[:image]).to include("を入力してください")
-    end
+      it "入力欄が全て入力されていた場合は登録できること" do
+        user = create(:user)
+        category = create(:category)
+        place = create(:place)
+        post = build(:post, user_id: user.id, category_id: category.id, place_id: place.id)
+        expect(post).to be_valid
+      end
 
-    it "textが無い場合は登録できないこと" do
-      post = build(:post, text: "")
-      post.valid?
-      expect(post.errors[:text]).to include("を入力してください")
     end
+    
+    context 'postを保存できない場合' do
 
-    it "user_idが無い場合は登録できないこと" do
-      post = build(:post, user_id: "")
-      post.valid?
-      expect(post.errors[:user]).to include("を入力してください")
-    end
+      it "titleが無い場合は登録できないこと" do
+        post = build(:post, title: "")
+        post.valid?
+        expect(post.errors[:title]).to include("を入力してください")
+      end
 
-    it "category_idが無い場合は登録できないこと" do
-      post = build(:post, category_id: "")
-      post.valid?
-      expect(post.errors[:category]).to include("を入力してください")
-    end
-  
-    it "place_idが無い場合は登録できないこと" do
-      post = build(:post, place_id: "")
-      post.valid?
-      expect(post.errors[:place]).to include("を入力してください")
+      it "imageが無い場合は登録できないこと" do
+        post = build(:post, image: "")
+        post.valid?
+        expect(post.errors[:image]).to include("を入力してください")
+      end
+
+      it "textが無い場合は登録できないこと" do
+        post = build(:post, text: "")
+        post.valid?
+        expect(post.errors[:text]).to include("を入力してください")
+      end
+
+      it "user_idが無い場合は登録できないこと" do
+        post = build(:post, user_id: "")
+        post.valid?
+        expect(post.errors[:user]).to include("を入力してください")
+      end
+
+      it "category_idが無い場合は登録できないこと" do
+        post = build(:post, category_id: "")
+        post.valid?
+        expect(post.errors[:category]).to include("を入力してください")
+      end
+    
+      it "place_idが無い場合は登録できないこと" do
+        post = build(:post, place_id: "")
+        post.valid?
+        expect(post.errors[:place]).to include("を入力してください")
+      end
+    
     end
 
   end
