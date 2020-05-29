074ffb86060687b9b0db57c177700a63ab764d57
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-29 17:10:46 +0900

    post.rb test imageが無い場合は登録できないこと
    pass済

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,12 @@ describe Post do
       post.valid?
       expect(post.errors[:title]).to include("を入力してください")
     end
+
+    it "imageが無い場合は登録できないこと" do
+      post = build(:post, image: "")
+      post.valid?
+      expect(post.errors[:image]).to include("を入力してください")
+    end
   
   end
 end
