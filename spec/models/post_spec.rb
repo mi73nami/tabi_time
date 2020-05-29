4b65e244783a3cd0d2f73800c2a004c8398a03f0
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-05-29 17:13:13 +0900

    post.rb test textが無い場合は登録できないこと
    pass済

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,6 +13,12 @@ describe Post do
       post.valid?
       expect(post.errors[:image]).to include("を入力してください")
     end
+
+    it "textが無い場合は登録できないこと" do
+      post = build(:post, text: "")
+      post.valid?
+      expect(post.errors[:text]).to include("を入力してください")
+    end
   
   end
 end
